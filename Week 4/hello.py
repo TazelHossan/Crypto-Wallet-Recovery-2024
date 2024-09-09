4ad7b3074f5e93c066d1cdf1074c5352c61bddc4
Tazel Hossan <59133879+TazelHossan@users.noreply.github.com>
2024-09-09 12:14:35 +0300

    Delete Week 4 directory

--- Week 4/hello.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-# Input function aloows for prompt the user for a cetain value which can be used in scripts
-name = input("Please enter your name: ")
-print("Hello, " + name)
--- Week 4/variables.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-print("HOME: " + os.environ.get("HOME", ""))
-print("SHELL: " + os.environ.get("SHELL", ""))
-# export FRUIT=Pineapple
-print("FRUIT: " + os.environ.get("FRUIT", ""))
